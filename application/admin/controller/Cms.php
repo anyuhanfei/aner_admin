247e0a6cbd60a1e55c416bc77e91ec23b7e0c8b5
暗语寒飞 <1223050251@qq.com>
2019-03-28 14:59:16 +0800

    Update Cms.php

--- application/admin/controller/Cms.php
+++ application/admin/controller/Cms.php
@@ -6,6 +6,7 @@ use think\Request;
 use think\Loader;
 
 use app\admin\model\CmsCategory;
+use app\admin\model\CmsTag;
 
 use app\admin\controller\Base;
 
@@ -72,6 +73,79 @@ class Cms extends Base{
             return json_data(1, '', '添加成功！');
         }else{
             return json_data(2, '', '添加失败！');
+        }
+    }
+    
+    /**
+     * 文章标签列表
+     *
+     * @return void
+     */
+    public function tag(){
+        $list = CmsTag::order('tag_id desc')->paginate(10, false,['query'=>request()->param()]);
+        $this->assign('list', $list);
+        return $this->fetch('Cms/cms_tag');
+    }
+
+    /**
+     * 文章标签添加提交
+     *
+     * @return void
+     */
+    public function tag_add_submit(){
+        $tag_name = Request::instance()->param('tag_name', '');
+        $validate = Loader::validate('CmsTag');
+        if(!$validate->scene('add')->check(['tag_name'=> $tag_name])){
+            return json_data(2, '', $validate->getError());
+        }
+        $model = new CmsTag(['tag_name'=> $tag_name]);
+        $res = $model->allowField(true)->save();
+        if($res){
+            LogAdminOperation::create_data(Session::get('admin')->admin_id, '添加了一条文章标签，名称为' . $tag_name);
+            return json_data(1, '', '添加成功！');
+        }else{
+            return json_data(2, '', '添加失败！');
+        }
+    }
+
+    /**
+     * 文章标签修改提交
+     *
+     * @return void
+     */
+    public function tag_update_submit(){
+        $data = Request::instance()->param();
+        $validate = Loader::validate('CmsTag');
+        if(!$validate->scene('update')->check($data)){
+            return json_data(2, '', $validate->getError());
+        }
+        $model = new CmsTag();
+        $res = $model->allowField(true)->save($data, ['id'=> $data['id']]);
+        if($res){
+            LogAdminOperation::create_data(Session::get('admin')->admin_id, '修改了一条文章标签，名称为' . $data['tag_name']);
+            return json_data(1, '', '修改成功！');
+        }else{
+            return json_data(2, '', '修改失败！');
+        }
+    }
+
+    /**
+     * 文章标签删除提交
+     *
+     * @return void
+     */
+    public function tag_delete_submit(){
+        $id = Request::instance()->param('id', 0);
+        $model = CmsTag::get($id);
+        if(!$model){
+            return json_data(2, '', '系统错误！');
+        }
+        $res = CmsTag::destroy($id);
+        if($res){
+            LogAdminOperation::create_data(Session::get('admin')->admin_id, '删除了一条文章标签，名称为' . $model->tag_name);
+            return json_data(1, '', '删除成功！');
+        }else{
+            return json_data(2, '', '删除失败！');
         }
     }
 }
