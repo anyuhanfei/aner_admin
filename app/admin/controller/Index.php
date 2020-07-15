a6bc0c5d6878c950577e393b32c7f9cd31d77f30
anyuhanfei <1223050251@qq.com>
2020-07-15 10:05:57 +0800

    中文测试, 失败了

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -2,12 +2,30 @@
 namespace app\admin\controller;
 
 use think\facade\View;
+use think\facade\Db;
 
 use app\admin\controller\Admin;
+
+use app\admin\model\中文测试;
 
 class Index extends Admin{
 
     public function index(){
+        Db::name('中文测试')->insert(['姓名'=> 'a', '创建时间'=> date("Y-m-d H:i:s", time())]);
+        // 中文测试::create(['姓名'=> 'a', '创建时间'=> date("Y-m-d H:i:s", time())]);
+        // var_dump(中文测试::)
         return View::fetch();
     }
 }
+
+
+/**
+
+CREATE TABLE `中文测试` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `姓名` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `创建时间` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ */
---
+++ app/admin/model/中文测试.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\admin\model;
+
+use think\Model;
+
+
+class 中文测试 extends Model{
+    protected $table = "中文测试";
+    protected $pk = 'id';
+}
